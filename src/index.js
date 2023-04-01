140e5779fd0ced62452235d51a5debe5c9456846
Clifton Gockul <clifga@gmail.com>
2023-04-01 12:55:28 -0400

    completed ish

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log('%c HI', 'color: firebrick')
+const dogContainer = document.querySelector('#dog-image-container')
+const ulDog = document.querySelector('#dog-breeds')
+const dropdown = document.getElementById('breed-dropdown')
+
+fetch('https://dog.ceo/api/breeds/image/random/4')
+.then(res => res.json())
+.then(data => renderDogs(data))
+
+const renderDogs = array => {
+    array.message.forEach(element => {
+        dogImg = document.createElement('img')
+        dogImg.src = element
+        dogContainer.append(dogImg)
+    })
+}
+
+fetch('https://dog.ceo/api/breeds/list/all').then(res => res.json()).then(data => {
+    let dogNames = Object.keys(data.message)
+    dogList(dogNames)
+})
+
+const dogList = array => {
+    array.forEach(element => {
+        let dogli = document.createElement('li')
+        dogli.textContent = element
+        ulDog.append(dogli)
+        dogli.addEventListener('click', (e)=>{
+            e.target.style.color = "red"
+        })
+    
+    dropdown.addEventListener('change', (e) => {
+            let letter = e.target.value
+            let filterdogs = array.filter(dog => dog.startsWith(letter))
+            ulDog.innerHTML=''
+            dogList(filterdogs)
+        })       
+    })
+}
+
